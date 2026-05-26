5b0852c9191ba7667450744430b0f74ad8e6d9d7
atulb8560@gmail.com <atulb8560@gmail.com>
2026-05-26 16:17:57 +0530

    SEO update

--- app/app/sitemap.ts
+++ app/app/sitemap.ts
@@ -3,6 +3,8 @@ export default function sitemap() {
     {
       url: "https://takshashilaeducationfoundation.org",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
   ];
 }
